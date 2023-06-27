9f29f55294432232a1e8eb4fc54331f81446b366
nisreen <nisokh7@gmail.com>
2023-06-27 10:34:42 +0200

    commit tt

--- database.sql
+++ database.sql
@@ -1,35 +1,64 @@
 DROP TABLE IF EXISTS genre_movie;
+
 DROP TABLE IF EXISTS movie;
+
 DROP TABLE IF EXISTS genre;
 
+CREATE TABLE
+    movie (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        title VARCHAR(255) NOT NULL,
+        resume TEXT,
+        released DATE,
+        length INT
+    );
 
-CREATE TABLE movie (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    resume TEXT,
-    released DATE,
-    length INT
-);
+CREATE TABLE
+    genre (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        label VARCHAR(255)
+    );
 
-CREATE TABLE genre (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    label VARCHAR(255)
-);
+CREATE TABLE
+    genre_movie (
+        id_movie INT,
+        id_genre INT,
+        PRIMARY KEY (id_movie, id_genre),
+        Foreign Key (id_movie) REFERENCES movie(id) ON DELETE CASCADE,
+        Foreign Key (id_genre) REFERENCES genre(id) ON DELETE CASCADE
+    );
 
-CREATE TABLE genre_movie (
-    id_movie INT,
-    id_genre INT,
-    PRIMARY KEY (id_movie,id_genre),
-    Foreign Key (id_movie) REFERENCES movie(id),
-    Foreign Key (id_genre) REFERENCES genre(id)
-);
+INSERT INTO
+    movie (title, resume, released, length)
+VALUES (
+        'The godfather',
+        'a mafia movie',
+        '1972-10-18',
+        175
+    ), (
+        'The godfather 2',
+        'a mafia movie sequel',
+        '1974-12-20',
+        202
+    ), (
+        'Star Wars',
+        'a jedi movie',
+        '1977-05-25',
+        121
+    ), (
+        'Howl\'s Moving Castle',
+        'a japanese animation film',
+        '2004-11-20',
+        119
+    );
 
-INSERT INTO movie (title,resume,released,length) VALUES
-('The godfather', 'a mafia movie', '1972-10-18', 175),
-('The godfather 2', 'a mafia movie sequel', '1974-12-20', 202),
-('Star Wars', 'a jedi movie', '1977-05-25', 121),
-('Howl\'s Moving Castle', 'a japanese animation film', '2004-11-20', 119);
+INSERT INTO genre (label)
+VALUES ('Horror'), ('Sci-fi'), ('Romance'), ('Thriller'), ('Action');
 
-INSERT INTO genre (label) VALUES ('Horror'), ('Sci-fi'),('Romance'),('Thriller'), ('Action');
+INSERT INTO
+    genre_movie(id_movie, id_genre)
+VALUES (1, 1), (2, 1), (3, 2), (3, 5), (4, 5), (4, 3);
 
-INSERT INTO genre_movie(id_movie,id_genre) VALUES (1,1), (2,1), (3,2), (3,5), (4,5),(4,3);
+select title, genre.label from movie
+left join genre_movie on movie.id=genre_movie.id_movie
+left join genre on genre.id=genre_movie.id_genre;
--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Genre;
 use App\Repository\GenreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+#[Route('/api/genre')]
 
 class GenreController extends AbstractController
 {
@@ -13,10 +18,63 @@ class GenreController extends AbstractController
     {
 
     }
-    #[Route('/genre')]
-    public function index()
+    #[Route(methods: 'GET')]
+    public function all(): Response
     {
+        $genre = $this->genrep->findAll();
 
-        return $this->json();
+        return $this->json($genre);
+    }
+
+    #[Route('/{id}', methods: 'GET')]
+    public function one(int $id): JsonResponse
+    {
+        $genre = $this->genrep->findById($id);
+        if ($genre == null) {
+            return $this->json('Resource Not Found', 404);
+
+        }
+        return $this->json($genre);
+    }
+    #[Route('/{id}', methods: 'DELETE')]
+    public function delete(int $id): JsonResponse
+    {
+        $genre = $this->genrep->findById($id);
+        if ($genre == null) {
+            return $this->json('Resource Not Found', 404);
+
+        }
+        $this->genrep->delete($id);
+
+        return $this->json(null, 204);
+    }
+
+
+    #[Route(methods: 'POST')]
+    public function add(Request $request, SerializerInterface $serializer)
+    {
+        // $data = $request->toArray();
+        // $movie = new Movie($data['title'], $data['resume'], new \DateTime($data['released']), $data['length']);
+        $genre = $serializer->deserialize($request->getContent(), Movie::class, 'json');
+        $this->genrep->persist($genre);
+        return $this->json($genre, 201);
+
+    }
+
+    #[Route('/{id}', methods: 'PATCH')]
+    public function update(int $id, Request $request, SerializerInterface $serializer)
+    {
+        // $data = $request->toArray();
+        // $movie = new Movie($data['title'], $data['resume'], new \DateTime($data['released']), $data['length']);
+        $genre = $this->genrep->findById($id);
+        if ($genre == null) {
+            return $this->json('Resource Not Found', 404);
+        }
+        $serializer->deserialize($request->getContent(), Genre::class, 'json', [
+            'object_to_populate' => $genre
+        ]);
+        $this->genrep->update($genre);
+        return $this->json($genre, 201);
+
     }
 }
